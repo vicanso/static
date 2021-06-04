9ad0e519781cc2ea63800f15b49c000c51397791
vicanso <vicansocanbico@gmail.com>
2021-06-04 21:10:44 +0800

    feat: support default index handler

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # static
 
-静态文件服务，提供对静态文件的HTTP访问。
+静态文件服务，提供对静态文件的HTTP访问。启动方式如下：
 
 ```bash
-
+docker run -it -d --restart=always \
+  -v /web:/static:ro \
+  -p 3000:3000 \
+  vicanso/static
 ```
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"os"
+	"time"
 
 	"github.com/vicanso/elton"
 	"github.com/vicanso/elton/middleware"
@@ -12,21 +13,32 @@ func main() {
 	staticPath := os.Getenv("STATIC")
 	e := elton.New()
 
+	e.Use(middleware.NewDefaultCompress())
+
 	sf := new(middleware.FS)
 	e.GET("/ping", func(c *elton.Context) error {
 		c.BodyBuffer = bytes.NewBufferString("pong")
 		return nil
 	})
+	e.GET("/", func(c *elton.Context) (err error) {
+		r, err := sf.NewReader(staticPath + "/index.html")
+		if err != nil {
+			return
+		}
+		c.SetContentTypeByExt(".html")
+		c.Body = r
+		return
+	})
 	// static file route
 	e.GET("/*", middleware.NewStaticServe(sf, middleware.StaticServeConfig{
 		Path: staticPath,
 		// 客户端缓存一年
-		MaxAge: 365 * 24 * 3600,
+		MaxAge: 365 * 24 * time.Hour,
 		// 缓存服务器缓存一个小时
-		SMaxAge:             60 * 60,
+		SMaxAge:             time.Hour,
 		DenyQueryString:     true,
 		DisableLastModified: true,
-		// 如果使用packr，它不支持Stat，因此需要用强ETag
+		// 如果使用内存式文件存储，不支持Stat，因此需要用强ETag
 		EnableStrongETag: true,
 	}))
 
