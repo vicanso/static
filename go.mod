7d9ea94cbc6b9ed1fc523ec357897632b1ee5c1e
vicanso <vicansocanbico@gmail.com>
2022-06-11 20:53:49 +0800

    feat: support http cache for static file

--- go.mod
+++ go.mod
@@ -1,15 +1,18 @@
 module github.com/vicanso/static
 
-go 1.17
+go 1.18
 
-require github.com/vicanso/elton v1.9.2
+require (
+	github.com/allegro/bigcache/v3 v3.0.2
+	github.com/vicanso/elton v1.9.3
+)
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/tidwall/gjson v1.14.1 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
-	github.com/vicanso/hes v0.5.1 // indirect
+	github.com/vicanso/hes v0.6.0 // indirect
 	github.com/vicanso/intranet-ip v0.1.0 // indirect
 	github.com/vicanso/keygrip v1.2.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bytes"
+	"context"
+	"fmt"
 	"os"
 	"regexp"
 	"strconv"
@@ -9,15 +11,40 @@ import (
 
 	"log"
 
+	"github.com/allegro/bigcache/v3"
 	"github.com/vicanso/elton"
 	"github.com/vicanso/elton/middleware"
 )
+
+type httpCache struct {
+	c *bigcache.BigCache
+}
+
+func (hc *httpCache) Get(ctx context.Context, key string) ([]byte, error) {
+	buf, err := hc.c.Get(key)
+	if err != nil && err != bigcache.ErrEntryNotFound {
+		return nil, err
+	}
+	return buf, nil
+}
+
+func (hc *httpCache) Set(ctx context.Context, key string, data []byte, ttl time.Duration) error {
+	return hc.c.Set(key, data)
+}
 
 func main() {
 	staticPath := os.Getenv("STATIC")
 	compressLevel, _ := strconv.Atoi(os.Getenv("CMP_LEVEL"))
 	minLength, _ := strconv.Atoi(os.Getenv("CMP_MIN_LENGTH"))
-	checker, _ := regexp.Compile(os.Getenv("CMP_CONTENT_TYPE"))
+	var checker *regexp.Regexp
+	contentType := os.Getenv("CMP_CONTENT_TYPE")
+	if contentType != "" {
+		checker, _ = regexp.Compile(contentType)
+	}
+	cacheTTL, _ := time.ParseDuration(os.Getenv("CACHE_TTL"))
+	if cacheTTL == 0 {
+		cacheTTL = 10 * time.Minute
+	}
 	e := elton.New()
 
 	e.Use(middleware.NewLogger(middleware.LoggerConfig{
@@ -27,21 +54,25 @@ func main() {
 		Format: middleware.LoggerCombined,
 	}))
 	e.Use(middleware.NewDefaultFresh())
-	e.Use(middleware.NewDefaultETag())
+
+	var compressor middleware.CacheCompressor
 	if compressLevel != 0 {
-		config := middleware.NewCompressConfig(
-			&middleware.BrCompressor{
-				MinLength: minLength,
-				Level:     compressLevel,
-			},
-			&middleware.GzipCompressor{
-				MinLength: minLength,
-				Level:     compressLevel,
-			},
-		)
-		config.Checker = checker
-		e.Use(middleware.NewCompress(config))
+		compressor = &middleware.CacheBrCompressor{
+			Level:         compressLevel,
+			MinLength:     minLength,
+			ContentRegexp: checker,
+		}
 	}
+	// 缓存直接使用10分钟
+	// 静态文件有版本号，10分钟短缓存不影响
+	cache, _ := bigcache.NewBigCache(bigcache.DefaultConfig(cacheTTL))
+
+	e.Use(middleware.NewCache(middleware.CacheConfig{
+		Store: &httpCache{
+			c: cache,
+		},
+		Compressor: compressor,
+	}))
 
 	sf := new(middleware.FS)
 	e.GET("/ping", func(c *elton.Context) error {
@@ -66,7 +97,12 @@ func main() {
 		SMaxAge: time.Hour,
 		// 禁止访问隐藏文件
 		DenyDot: true,
+		// 启用强ETag
+		EnableStrongETag: true,
 	}))
+	msg := fmt.Sprintf("path:%s, compress(level:%d, minLength:%d, contentType:%s)", staticPath, compressLevel, minLength, contentType)
+	log.Println(msg)
+	log.Println("server is running")
 
 	err := e.ListenAndServe(":3000")
 	if err != nil {
